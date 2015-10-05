4ab4d6be2aadecd3b1e1b446fec97d52bf7de24a
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-10-05 14:25:10 -0300

    v0.0.3

--- mailgunlog/__init__.py
+++ mailgunlog/__init__.py
@@ -4,7 +4,7 @@ Retrive Mailgun logs for a given domain.
 '''
 
 __title__ = 'mailgunlog'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Getup Cloud'
 __license__ = 'Apache License 2.0'
 __copyright__ = 'Copyright 2015 Getup Cloud'
